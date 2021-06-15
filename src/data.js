9a985776c1ad8dfd03d4ed6acb521cb864266ad2
Barnabas Molnar <hanta911@gmail.com>
2021-06-15 19:04:26 +0200

    create data.js + Basic set up

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const colton = {
+    name: 'Colton Smith',
+    title: "Verified Buyer",
+    text: "Weneeded the same printed design as the one we had ordered a week prior.Not only did they find the original order, but we also received it in time.Excellent!"
+
+}
+
+const irene = {
+    name: 'Irene Roberts',
+    title: "Verified Buyer",
+    text: "Customer service is alwaysexcellent and very quick turn around. Completely delighted with thesimplicity of the purchase and the speed of delivery."
+
+}
+
+const anne = {
+    name: 'Anne Wallace',
+    title: "Verified Buyer",
+    text: "Put an order with this company and can only praise them for the very high standard.Will definitely use them again and recommend them to everyone!"
+
+}
